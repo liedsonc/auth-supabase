0272da21fdf207156d4d394c121c6f766ca7fb1a
liedsonc <liedsoncanal@gmail.com>
2026-02-11 20:54:44 +0000

    feat: add package exports and core-auth-kit re-exports

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+export * from '@liedsonc/core-auth-kit';
+
+export * from './supabase-adapter';
+export * from './supabase-client';
+export * from './types';
+export * from './env-config';
+export * from './utils/password';
+export * from './utils/token';
+export * from './utils/resend-email';
